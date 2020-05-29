c1157e747cc9fd9a4b0a2a07b6d881e3408ee589
HeroPPPath <2661041991@qq.com>
2020-05-29 13:00:37 +0800

    jsp

--- src/main/java/springbootjsp/zzq/App.java
+++ src/main/java/springbootjsp/zzq/App.java
@@ -1,12 +1,13 @@
 package springbootjsp.zzq;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
-// 这里扫描controller里的文件，目录如果不一样记得改
-@ComponentScan(value = { "springbootjsp.zzq.controller" })
+//@EnableAutoConfiguration
+//// 这里扫描controller里的文件，目录如果不一样记得改
+//@ComponentScan(value = { "springbootjsp.zzq.controller"})
+
+@SpringBootApplication
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
